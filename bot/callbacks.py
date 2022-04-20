3aaa472c38b99367a88e1b4736774eacd38c76d1
Dheisom Gomes <dheisomgomes0@gmail.com>
2022-04-20 19:27:25 -0300

    Revert "Revert "Save point:""
    This reverts commit 18fac8314d65f6d51e86911f0162b38088e3e762.

--- bot/callbacks.py
+++ bot/callbacks.py
@@ -1,7 +1,6 @@
-from . import app
-from pyrogram import Client
+from . import app, posts
 from pyrogram.types import (
-    CallbackQuery, Message, InlineKeyboardMarkup, InlineKeyboardButton
+    CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton
 )
 
 
@@ -10,38 +9,80 @@ async def callback_handler(_, callback: CallbackQuery):
     cmd = callback.data.split()
     if cmd[0] == 'menu':
         await menu(callback)
-    if cmd[0] == 'deleteList':
+    elif cmd[0] == 'deleteList':
         await delete_list(callback)
     elif cmd[0] == 'delete':
         await delete_service(callback, int(cmd[1]))
+    elif cmd[0] == 'styleList':
+        await style_list(callback)
+    elif cmd[0] == 'chooseStyle':
+        await style_choose(callback, cmd[1])
 
 
 async def menu(callback: CallbackQuery):
     await callback.edit_message_text(
         "Select an option:",
         reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Set post style", "setStyle")],
+            [InlineKeyboardButton("Set post style", "styleList")],
             [InlineKeyboardButton("Delete a service", "deleteList")],
             [InlineKeyboardButton("Get informations", "getInformations")],
         ])
     )
 
 
+async def style_list(callback: CallbackQuery):
+    await callback.edit_message_text(
+        "Select the part of the post you want to select the style:",
+        reply_markup=InlineKeyboardMarkup(
+            list(map(
+                lambda v: [InlineKeyboardButton(
+                    text=v.title(),
+                    callback_data=f'chooseStyle {v}'
+                )],
+                posts.Post.style.keys()
+            ))
+        )
+    )
+
+
+async def style_choose(callback: CallbackQuery, position: str):
+    lines = list(map(
+        lambda style: [InlineKeyboardButton(
+            text=style['name'],
+            callback_data=f'setStyle {position} {posts.styles.index(style)}'
+        )],
+        posts.styles
+    ))
+    await callback.edit_message_text(
+        f"Choose one style for the {position}:",
+        reply_markup=InlineKeyboardMarkup(lines)
+    )
+
+
+async def style_set(callback: CallbackQuery, position: str, style: int):
+    pass
+
+
 async def delete_list(callback: CallbackQuery):
     user_id = app.database.getUser(callback.message.chat.id).id
     services = app.database.getServicesOfUser(user_id)
-    rows = []
+    lines = []
     for service in services:
-        rows.append([
+        lines.append([
             InlineKeyboardButton(service.title, f'delete {service.id}')
         ])
-    if len(rows) == 0:
+    if len(lines) == 0:
         await callback.answer("You don't have any service registered!")
-        try: await menu(callback)
-        except: pass
+        try:
+            await menu(callback)
+        except Exception:
+            pass
         return
-    rows.append([InlineKeyboardButton("Go back", "menu")])
-    await callback.edit_message_text("Choose a service:", reply_markup=InlineKeyboardMarkup(rows))
+    lines.append([InlineKeyboardButton("Go back", "menu")])
+    await callback.edit_message_text(
+        "Choose a service:",
+        reply_markup=InlineKeyboardMarkup(lines)
+    )
 
 
 async def delete_service(callback: CallbackQuery, service_id: int):
